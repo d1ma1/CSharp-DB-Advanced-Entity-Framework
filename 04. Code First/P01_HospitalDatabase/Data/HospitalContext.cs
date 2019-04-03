d8985c35e9f305db86dbf7c6ce547cc332ebd39b
Dima Dimova <dima.dimova@mail.bg>
2019-04-03 20:41:23 +0300

    Add files via upload

---
+++ 04. Code First/P01_HospitalDatabase/Data/HospitalContext.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_HospitalDatabase.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data
+{
+    public class HospitalContext: DbContext
+    {
+        public DbSet<Visitation> Visitations { get; set; }
+
+        public DbSet<Diagnose> Diagnoses { get; set; }
+
+        public DbSet<Patient> Patients { get; set; }
+
+        public DbSet<Medicament> Medicaments { get; set; }
+
+        public DbSet<PatientMedicament> PatientMedicaments { get; set; }
+
+        public DbSet<Doctor> Doctors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DIMA\\SQLEXPRESS;Database=Hospital;Integrated Security=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Patient>(entity => {
+
+                 entity.HasKey(p => p.PatientId);
+
+                 entity.Property(p => p.FirstName)
+                     .HasMaxLength(50)
+                     .IsUnicode(true);
+
+                 entity.Property(p => p.LastName)
+                    .HasMaxLength(50)
+                    .IsUnicode(true);
+
+                 entity.Property(p => p.Address)
+                    .HasMaxLength(250)
+                    .IsUnicode(true);
+
+                 entity.Property(p => p.Email)
+                    .HasMaxLength(80)
+                    .IsUnicode(true);
+
+                entity.HasMany(v => v.Visitations)
+                    .WithOne(p => p.Patient);
+
+                entity.HasMany(v => v.Diagnoses)
+                    .WithOne(p => p.Patient);
+
+                //entity.HasMany(v => v.Prescriptions)
+                //    .WithOne(p => p.Patient);
+            });
+
+            modelBuilder.Entity<Visitation>(entity=>{
+
+                entity.HasKey(v => v.VisitationId);
+
+                entity.Property(v => v.Comments)
+                    .HasMaxLength(250)
+                    .IsUnicode(true);
+            });
+
+            modelBuilder.Entity<Diagnose>(entity =>
+            {
+                entity.HasKey(d => d.DiagnoseId);
+
+                entity.Property(d => d.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode();
+
+                entity.Property(d => d.Comments)
+                    .HasMaxLength(250)
+                    .IsUnicode();
+            });
+
+            modelBuilder.Entity<Medicament>(entity =>
+            {
+                entity.HasKey(m => m.MedicamentId);
+
+                entity.Property(m => m.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode();
+
+                //entity.HasMany(pm => pm.Prescriptions)
+                //    .WithOne(m => m.Medicament);
+            });
+
+            modelBuilder.Entity<PatientMedicament>(entity =>
+            {
+                entity.HasKey(s => new { s.PatientId, s.MedicamentId });
+
+                entity.HasOne(p => p.Patient)
+                    .WithMany(pm => pm.Prescriptions)
+                    .HasForeignKey(p=>p.PatientId);
+
+                entity.HasOne(m => m.Medicament)
+                    .WithMany(pm => pm.Prescriptions)
+                     .HasForeignKey(m => m.MedicamentId);
+            });
+
+            modelBuilder.Entity<Doctor>(entity =>
+            {
+                entity.HasKey(d => d.DoctorId);
+
+                entity.Property(d => d.Name)
+                    .HasMaxLength(100)
+                    .IsUnicode();
+
+                entity.Property(d => d.Specialty)
+                    .HasMaxLength(100)
+                    .IsUnicode();
+
+                entity.HasMany(v => v.Visitations)
+                    .WithOne(d => d.Doctor);
+            });
+        }
+    }
+}
+
---
+++ 04. Code First/P01_HospitalDatabase/Data/Models/Patient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class Patient
+    {
+
+        public Patient()
+        {
+            Visitations = new List<Visitation>();
+            Diagnoses = new List<Diagnose>();
+            Prescriptions = new List<PatientMedicament>();
+        }
+        public int PatientId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+
+        public bool HasInsurance { get; set; }
+
+        public ICollection<Diagnose> Diagnoses { get; set; }
+        public ICollection<Visitation> Visitations { get; set; }
+        public ICollection<PatientMedicament> Prescriptions { get; set; }
+    }
+}
